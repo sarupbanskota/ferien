f621ad4949f04868b6a7feeca11504f2241a58dd
Sarup Banskota <sbanskota08@gmail.com>
2016-04-04 14:33:41 +0800

    Link employees and requests

--- app/models/request.js
+++ app/models/request.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   from: DS.attr('date'),
   to: DS.attr('date'),
-  notes: DS.attr('string')
+  notes: DS.attr('string'),
+  employee: DS.belongsTo('employee')
 });
